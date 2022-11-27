f0a55029fcc9c93f1fd63dee0f6f029e2330a680
Ivan Hansen <ihk225@gmail.com>
2022-11-27 05:25:57 -0300

    Ex6 test

--- models/city.py
+++ models/city.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from os import getenv
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+import os
+STRG = os.environ.get('HBNB_TYPE_STORAGE')
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if STRG == 'db':
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey('states.id'))
         name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete, delete-orphan")
     else:
         state_id = ""
         name = ""
--- models/state.py
+++ models/state.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
-from models.city import City
+import models
+import os
+STRG = os.environ.get('HBNB_TYPE_STORAGE')
+
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    if STRG == 'db':
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         name = ""
 
         @property
         def cities(self):
-            """ Returns all cities corresponding to the given state """
-            from models import storage
-            ret_lst = []
-            for city in storage.all(City).values():
-                if self.id == city.state_id:
-                    ret_lst.append(city)
-            return ret_lst
+            """ Getter method """
+            from models.city import City
+            return [obj for obj in models.storage.all(City).values() if
+                    obj.state_id == self.id]
 
