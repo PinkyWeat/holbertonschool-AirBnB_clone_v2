b517342c33c22de2b98eac13888037d37baca6d3
Ivan Hansen <ihk225@gmail.com>
2022-11-27 16:30:43 -0300

    Test corrections

--- models/user.py
+++ models/user.py
@@ -7,8 +7,8 @@ from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
+    __tablename__ = 'users'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
         places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
         reviews = relationship("Review", backref="user", cascade="all, delete, delete-orphan")
         email = Column(String(128), nullable=False)
