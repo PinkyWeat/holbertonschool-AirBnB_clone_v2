0ef3dc1c4d7f5312dc4a1fd5b245e9f2d60d986d
Denisse Landau <denisselandau97@gmail.com>
2022-11-29 16:30:37 -0300

    port 5000

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,16 @@
 """Python Package 1"""
 from flask import Flask
 
-app=Flask(__name__)  # create Flask obj referencing self
 
-@app.route('/', strict_slashes=False)  #
+app = Flask(__name__)
+# create Flask obj referencing self
+
+
+@app.route('/', strict_slashes=False)
 def index():
     return "Hello HBNB!"
 
-if __name__ == '__main__':  #  check that's the route + run the app
+
+# check that's the route + run the app
+if __name__ == '__main__':
     app.run(port=5000)
