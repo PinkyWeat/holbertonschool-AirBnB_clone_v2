215f20befc23ee368d90cd4225987811482fefc1
Denisse Landau <denisselandau97@gmail.com>
2022-11-29 17:06:12 -0300

    routes

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Python Package 1"""
+from flask import Flask
+
+
+app = Flask(__name__)
+# create Flask obj referencing self
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    text = str(text)
+    if '_' in text:
+        text = text.replace('_')
+    return "C " + text
+
+
+# check that's the route + run the app
+if __name__ == '__main__':
+    app.run(port=5000)
