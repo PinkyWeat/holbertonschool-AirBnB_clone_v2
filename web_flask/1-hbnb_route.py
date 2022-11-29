21469dc0f356e8c710e63d25aeab63306b1dfda3
Denisse Landau <denisselandau97@gmail.com>
2022-11-29 16:42:28 -0300

    more routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python Package 1"""
+from flask import Flask
+
+
+app = Flask(__name__)
+# create Flask obj referencing self
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+# check that's the route + run the app
+if __name__ == '__main__':
+    app.run(port=5000)
