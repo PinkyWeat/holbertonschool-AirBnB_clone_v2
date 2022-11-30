0b29f2fafa28a30074af4766aea400bf3a583556
Denisse Landau <denisselandau97@gmail.com>
2022-11-30 17:23:04 -0300

    more variable rendering

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Python Package 1"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+# create Flask obj referencing self
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def index():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text=None):
+    return "C " + str(text).replace("_", " ")
+
+
+@app.route('/python/<text>')
+@app.route('/python')
+def py(text="is cool"):
+    return "Python " + str(text).replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def is_num(n):
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>")
+def number_template_route(n):
+    return render_template("5-number.html", variable=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def odd_or_even(n):
+    res = "even" if n / 2 == 0 else "odd"
+    return render_template("6-number_odd_or_even.html", variable=n, whatami=res)
+
+
+# check that's the route + run the app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
